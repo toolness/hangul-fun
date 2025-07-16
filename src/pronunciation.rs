ca6a91941933d83a228593e6315ffb5c26f2e531
Atul Varma <varmaa@gmail.com>
2025-07-15 20:22:43 -0400

    Add rules for ㄿ.

--- src/pronunciation.rs
+++ src/pronunciation.rs
@@ -144,6 +144,12 @@ fn apply_compound_consonant_rules(
         }
         (FinalConsonant('ᆳ'), _) => (FinalConsonant('ᆯ'), next_initial_consonant),
 
+        // Rules for ㄿ
+        (FinalConsonant('ᆵ'), Some(InitialConsonant('ᄋ'))) => {
+            (FinalConsonant('ᆯ'), Some(InitialConsonant('ᄑ')))
+        }
+        (FinalConsonant('ᆵ'), _) => (FinalConsonant('ᆸ'), next_initial_consonant),
+
         // TODO: Add the rest of them.
         _ => (final_consonant, next_initial_consonant),
     }
